0cef95d07b7f45a5568b5e497cf48b4738899782
Francisco_Gamero <126238831+FranciscoGamero@users.noreply.github.com>
2024-12-05 14:06:17 +0100

    Ejercicio Login

--- ejercicio_login/lib/login_page.dart
+++ ejercicio_login/lib/login_page.dart
@@ -16,11 +16,7 @@ class LoginPage extends StatelessWidget {
             children: [
               Column(
                 children: [
-                  const Image(
-                    image: AssetImage('images/path_logo.png'),
-                    width: 300,
-                    height: 100,
-                  ),
+                  Image.asset('images/path_logo.png', width: 200, height: 100),
                   const Text(
                     'Beautiful, Private Sharing',
                     style: TextStyle(
@@ -28,16 +24,62 @@ class LoginPage extends StatelessWidget {
                       fontSize: 15,
                     ),
                   ),
-                  ElevatedButton(
-                      onPressed: () {}, child:  const Text('Sign Up')),
-                  Column(
-                    children: [
-                      const Text('Already have Path account?'),
-                      ElevatedButton(
+                  Container(
+                    margin: const EdgeInsets.only(top: 200),
+                    child: SizedBox(
+                      width: 300,
+                      child: FloatingActionButton(
+                          onPressed: () {},
+                          elevation: 0,
+                          child: const Text('Sign up',
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFE62F16)))),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: const Text('Already have Path account?',
+                        style: TextStyle(
+                          color: Color.fromRGBO(224, 224, 224, 0.808),
+                          fontSize: 15,
+                        )),
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: OutlinedButton(
                         onPressed: () {},
-                        child: const Text('Log In'),
+                        child: const Text(
+                          'Log In',
+                          style: TextStyle(
+                              color: Color.fromRGBO(224, 224, 224, 0.808)),
+                        )),
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: const TextSpan(
+                        text: 'By using Path, you agree to our ',
+                        style: TextStyle(
+                            fontSize: 17,
+                            color: Color.fromARGB(129, 197, 197, 197)),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: 'Terms of Service',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 110, 255),
+                                  decoration: TextDecoration.underline)),
+                          TextSpan(text: ' and '),
+                          TextSpan(
+                              text: 'Privace Policy',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 110, 255),
+                                  decoration: TextDecoration.underline)),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ],
               ),
